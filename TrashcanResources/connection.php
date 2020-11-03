c602c7be21f25616cb9a3e48435a818a0e8febb7
k-connolly <49733412+k-connolly@users.noreply.github.com>
2020-11-03 12:04:19 -0600

    Reorganizing to Project Trashcan Folder

--- TrashcanResources/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$username = "root";
-$pass = "";
-$host = "localhost";
-$db_name = "trashcan";
-$con = mysqli_connect ($host, $username, $pass);
-$db = mysqli_select_db ( $con, $db_name );
-?>
